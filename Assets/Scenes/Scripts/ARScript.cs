0d1b5dde6af18f0c769219300970aead8963b786
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2020-02-06 15:20:25 -0400

    Add everything

---
+++ Assets/Scenes/Scripts/ARScript.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARScript : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public ARRaycastManager raycastManager;
+
+    
+    private Camera _mainCamera;
+    private Vector3 _screenCenter;
+    private bool _poseIsValid;
+    private Pose _pose;
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        if (Camera.main != null)
+        {
+            _mainCamera = Camera.main;
+            _screenCenter = _mainCamera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        }
+    }
+    
+    private void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+    
+    private void UpdatePlacementPose()
+    {
+        raycastManager.Raycast(_screenCenter, _hits, TrackableType.Planes);
+        _poseIsValid = _hits.Count > 0;
+        
+        if (_poseIsValid)
+        {
+            _pose = _hits[0].pose;
+            var cameraForward = _mainCamera.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z);
+            _pose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (_poseIsValid)
+        {
+            placementIndicator.transform.SetPositionAndRotation(_pose.position, _pose.rotation);
+        }
+    }
+
+
+}
--- Assets/Scenes/Scripts/UIController.cs
+++ Assets/Scenes/Scripts/UIController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class UIController : MonoBehaviour
 {
@@ -13,29 +14,30 @@ public class UIController : MonoBehaviour
     public Button[] topicButtons;
     public TextMeshProUGUI contentExplanation;
     public ContetTexts contentExplanationsTexts;
+    public GameObject panel;
+    public Button backButton2;
+    public GameObject questionButtonPrefab;
+    public GameObject questionContentArea;
     
     private int _currentScreen = 0; 
     
     public void Awake()
     {
+        for (var i = 0; i < screens.Length; i++)
+        {
+            screens[i].gameObject.SetActive(false);
+        }
+        
         SetupScreen();
+        InstantiateButtons();
         nextButton.onClick.AddListener(NextScreen);
         backButton.onClick.AddListener(LastScreen);
-        for (var i = 0; i < topicButtons.Length; i++)
-        {
-            var i1 = i;
-            topicButtons[i].onClick.AddListener(() =>
-            {
-                NextScreen();
-                var text = contentExplanationsTexts.texts[i1];
-                contentExplanation.text = text;
-            });
-        }
+        backButton2.onClick.AddListener(ActivatePanel);    
     }
 
     private void NextScreen()
     {
-        if (_currentScreen < screens.Length - 1)
+        if (_currentScreen < screens.Length)
         {
             screens[_currentScreen].SetActive(false);
             _currentScreen++;
@@ -55,6 +57,22 @@ public class UIController : MonoBehaviour
         Debug.Log(_currentScreen);
     }
 
+    private void InstantiateButtons()
+    {
+        for (var i = 0; i < contentExplanationsTexts.texts.Length; i++)
+        {
+            var instantiatedButton = Instantiate(questionButtonPrefab,questionContentArea.transform);
+            var button = instantiatedButton.GetComponent<Button>();
+
+            var i1 = i;
+            button.onClick.AddListener((() =>
+            {
+                NextScreen();
+                var text = contentExplanationsTexts.texts[i1];
+                contentExplanation.text = text;
+            }));
+        }
+    }
     private void SetupScreen()
     {
         switch (_currentScreen)
@@ -72,7 +90,20 @@ public class UIController : MonoBehaviour
             case 2:
                 screens[2].SetActive(true);
                 nextButton.gameObject.SetActive(true);
+                panel.SetActive(true);
+                break;
+            case 3:
+                nextButton.gameObject.SetActive(false);
+                panel.SetActive(false);
                 break;
         }
+    }
+    
+    private void ActivatePanel()
+    {
+        panel.SetActive(true);
+        nextButton.gameObject.SetActive(true);
+        _currentScreen--;
+        SetupScreen();
     }
 }

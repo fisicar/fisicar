3771b5da54bb34d0f9200feb7caa36293f87dd92
Lucas Lima <lucas.fairu@gmail.com>
2020-02-17 19:25:09 -0400

    Format text of models

--- Assets/Scripts/App/ModelUIController.cs
+++ Assets/Scripts/App/ModelUIController.cs
@@ -5,17 +5,37 @@ public class ModelUIController : MonoBehaviour
 {
     public TextMeshProUGUI positionText;
     public int index;
+    public bool printParenthesis = true;
+    public bool printX = true;
+    public bool printY = true;
+    public int floatPoints = 2;
 
     private void Awake()
     {
-        ProblemController.OnModelPositionUpdate+= OnModelPositionUpdate;
+        ProblemController.OnModelPositionUpdate += OnModelPositionUpdate;
     }
 
-    private void OnModelPositionUpdate(int index, Vector2 Position)
+    private void OnModelPositionUpdate(int index, Vector2 position)
     {
         if (this.index == index)
         {
-            positionText.text = Position.ToString();
+            var text = "";
+            var format = "F" + floatPoints;
+            if (printParenthesis)
+                text = "(";
+            if (printX)
+                text += position.x.ToString(format);
+            if (printY)
+            {
+                if (printX)
+                    text += ", ";
+                text += position.y.ToString(format);
+            }
+
+            if (printParenthesis)
+                text += ")";
+
+            positionText.text = text;
         }
     }
 }

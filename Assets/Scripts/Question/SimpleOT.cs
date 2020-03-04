1d3b5ff3c0fe1fe5c9073d9c19ef3993e1925c9e
Selthon Moura <lolseltnnm@hotmail.com>
2020-03-03 23:22:35 -0400

    Add SimpleOT Problem

---
+++ Assets/Scripts/Question/SimpleOT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Question;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Problems/Simple OT")]
+public class SimpleOT : Problem
+{
+    public Vector2 initialPosition;
+    public float finalHeight;
+    public float throwAngle;
+    public float initialVelocity;
+    public float gravity;
+    private SimpleMU _movementX;
+    private SimpleSecondMUV _movementY;
+
+    [ContextMenu("Process")]
+    
+    public override void Process()
+    {
+        _movementX = ScriptableObject.CreateInstance<SimpleMU>();
+        _movementY = ScriptableObject.CreateInstance<SimpleSecondMUV>();
+        _movementY.acceleration = gravity;
+        _movementY.initialPosition = initialPosition.y;
+        _movementY.initialVelocity = initialVelocity * Mathf.Sin(throwAngle * Mathf.Deg2Rad);
+        _movementY.finalPosition = finalHeight;
+        _movementX.initialPosition = initialPosition.x;
+        _movementX.velocity = initialVelocity * Mathf.Cos(throwAngle * Mathf.Deg2Rad);
+        _movementY.Process();
+        _movementX.finalPosition = _movementX.initialPosition + _movementX.velocity * _movementY.Answer;
+        _movementX.Process();
+        Answer = _movementY.Answer;
+        minValue = new Vector2(_movementX.minValue.x, _movementY.minValue.x);
+        maxValue = new Vector2(_movementX.maxValue.x, _movementY.maxValue.x);
+    }
+    public override float Evaluate(float normalizedValue)
+    {
+        Debug.Log("EvaluateY: " + _movementY.Evaluate(normalizedValue));
+        return _movementX.Evaluate(normalizedValue);
+    }
+    
+    public float EvaluateY(float normalizedValue)
+    {
+        return _movementY.Evaluate(normalizedValue);
+    }
+}

bec0eedb4da394990c70dc3b09799fd46e596c8a
Lucas Lima <lucas.fairu@gmail.com>
2020-03-09 20:30:48 -0400

    Make the text look at camera

---
+++ Assets/Scripts/App/LookToCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookToCamera : MonoBehaviour
+{
+    private static Camera _mainCamera;
+
+    private void Awake()
+    {
+        if (_mainCamera == null)
+            _mainCamera = Camera.main;
+        
+    }
+
+    void Update()
+    {
+        
+        transform.rotation = Quaternion.LookRotation(transform.position - _mainCamera.transform.position);
+        
+    }
+}

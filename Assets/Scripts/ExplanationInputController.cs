b3309c96c2fbfbd3314637b7b34cb74d521dc5a6
Selthon Moura <lolseltnnm@hotmail.com>
2020-04-30 20:13:55 -0400

    Refactor ExplanationScreen Prefab

---
+++ Assets/Scripts/ExplanationInputController.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplanationInputController : MonoBehaviour
+{
+    private ProblemDefinition _currentProblem;
+}
---
+++ Assets/Scripts/InputLine.cs
@@ -0,0 +1,9 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class InputLine : MonoBehaviour
+{
+    public TextMeshProUGUI title;
+    public TMP_InputField inputField;
+    public TextMeshProUGUI unit;
+}

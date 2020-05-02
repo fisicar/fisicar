985ee127682dfc226899df7463ec234b5757a957
Lucas Lima <lucas.fairu@gmail.com>
2020-05-02 15:34:44 -0400

    Update Problem UI text

--- Assets/Scripts/App/ProblemUIController.cs
+++ Assets/Scripts/App/ProblemUIController.cs
@@ -8,12 +8,27 @@ public class ProblemUIController : MonoBehaviour
 {
     public TextMeshProUGUI minValueText;
     public TextMeshProUGUI maxValueText;
+    
+    private bool _printParenthesis = true;
+    private bool _printX = true;
+    private bool _printY = true;
     private string _unit;
+    private string _velocityUnit;
 
     private void Awake()
     {
         ProblemController.OnMinMaxValueChange += OnMinMaxChange;
         ProblemController.OnUnitChange += OnUnitChange;
+        ProblemController.UpdateModelDetails += OnUpdateModelDetails;
+    }
+    
+    private void OnUpdateModelDetails(bool printParenthesis, bool printX, bool printY, string unit, string velocityUnit)
+    {
+        _printParenthesis = printParenthesis;
+        _printX = printX;
+        _printY = printY;
+        _unit = unit;
+        _velocityUnit = velocityUnit;
     }
 
     private void OnUnitChange(string unit)
@@ -24,8 +39,29 @@ public class ProblemUIController : MonoBehaviour
 
     private void OnMinMaxChange(Vector2 min, Vector2 max)
     {
-        minValueText.text = min.ToString() + _unit;
-        maxValueText.text = max.ToString() + _unit;
+        var text = "";
+        var format = "F" + 2;
+        if (_printX)
+            text += "S<sub>0</sub>" + (_printX && _printY ? "x" : "") + ": " + min.x.ToString(format) + _unit +
+                    Environment.NewLine;
+            
+        if (_printY)
+        {
+            text += "S<sub>0</sub>y: "+ min.y.ToString(format)+ _unit + Environment.NewLine;
+        }
         
+        minValueText.text = text;
+
+        var maxText = "";
+        if (_printX)
+            maxText += "S<sub>0</sub>" + (_printX && _printY ? "x" : "") + ": " + max.x.ToString(format) + _unit +
+                    Environment.NewLine;
+            
+        if (_printY)
+        {
+            maxText += "S<sub>0</sub>y: "+ max.y.ToString(format)+ _unit + Environment.NewLine;
+        }
+
+        maxValueText.text = maxText;
     }
 }
